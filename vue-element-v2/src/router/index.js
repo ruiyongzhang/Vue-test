81c9cc4a67a1c833611baf2c364a070c5d5d2804
ruiyongzhang <78482180+ruiyongzhang@users.noreply.github.com>
2023-07-17 09:00:19 +0800

    v2

---
+++ vue-element-v2/src/router/index.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '../views/Login'
+import Main from '../views/Main'
+import MemberList from '../views/Member/MemberList'
+import MemberLevel from '../views/Member/MemberLevel'
+import NotFound from '../views/NotFound'
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes:[
+    {
+      // 登陆页
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      // 首页
+      path: '/main/:name',
+      name: 'Main',
+      component: Main,
+      children: [
+        {
+          // 会员列表
+          path: '/member/list',
+          name: 'MemberList',
+          component: MemberList
+        },
+        {
+          // 会员等级
+          path: '/member/level/:id',
+          name: 'MemberLevel',
+          component: MemberLevel,
+          props: true
+        }
+      ]
+    },
+    {
+      path: '/goMain/:name',
+      redirect: '/main/:name'
+    },
+    {
+      path: '*',
+      component: NotFound
+    }
+  ]
+})
---
+++ vue-element-v2/src/views/Login.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <el-form ref="form" :model="form" :rules="rules" class="login-box">
+      <h3 class="login-title">欢迎登录</h3>
+      <el-form-item label="账号" prop="name">
+        <el-input type="text" placeholder="请输入用户名" v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" placeholder="请输入密码" v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitFrom('form')">登录</el-button>
+      </el-form-item>
+      
+    </el-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data(){
+      return{
+        form: {
+          name: '',
+          password: ''
+        },
+        rules:{
+          name: [{
+            required: true,
+            message: '请输入用户名',
+            trigger: 'blur'
+          }],
+          password: [{
+            required: true,
+            message: '请输入密码'
+          }]
+        }
+      }
+    },
+    methods:{
+      submitFrom(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            sessionStorage.setItem('isLogin','true');
+            this.$router.push({name: 'Main', params:{name: this.form.name}});
+          } else {
+            this.$message({
+              message: '用户名或密码错误',
+              type: 'warning'
+            })
+            return false;
+          }
+        })
+      }
+    }
+  };
+  
+  
+</script>
+
+<style lang="scss" scoped>
+  .login-box{
+    width:350px;
+    margin:150px auto;
+    border: 1px solid black;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0 0 30px #DCDFE6;
+  }
+  .login-title{
+    text-align: center;
+  }
+</style>
